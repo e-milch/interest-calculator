74369eb0a9f954a5615ae3333325c2b09f393d5d
Ella Milech <es.milech@gmail.com>
2021-09-24 08:43:14 +1000

    cleanup and abstracting functions

--- dom.js
+++ dom.js
@@ -14,3 +14,25 @@ export const getContributions = () => {
     .value.split(" ")
     .map((i) => Number(i));
 };
+
+const results = document.getElementById("results");
+
+export const displayInterest = (interestArr) => {
+  let interest;
+  results.append("interest each year");
+  for (let i = 0; i < interestArr.length; i++) {
+    interest = document.createElement("P");
+    interest.innerHTML = `Year ${i + 1} $${interestArr[i]}`;
+    results.append(interest);
+  }
+};
+
+export const displayBalance = (balanceArr) => {
+  let balance;
+  results.append("Balance at start of each year");
+  for (let i = 0; i < balanceArr.length; i++) {
+    balance = document.createElement("P");
+    balance.innerHTML = `Year ${i + 1} $${balanceArr[i]}`;
+    results.append(balance);
+  }
+};
--- interest.js
+++ interest.js
@@ -1,25 +1,11 @@
-// function interest(feesPerYear, interestRate) {
-//   // feesPerYear is an array
-//   // set feesPerYear[0] as the initial value
-//   let a = feesPerYear[0];
-
-//   for (let i = 1; i < feesPerYear.length; i++) {
-//     console.log(a);
-//     a += a * (interestRate / feesPerYear.length) + feesPerYear[i];
-//   }
-
-//   return a;
-// }
-
-// let feeArray = [
-//   21000, 22261.5, 22261.5, 25656.5, 25656.5, 28275.5, 28272.5, 31379.5, 31379.5,
-//   33707.5, 33707.5, 36229.5, 36229.5,
-// ];
-// let interestRate = 0.081;
-
-// console.log(interest(feeArray, interestRate));
-
-import { getStartValue, getRate, getContributions, calcButton } from "./dom.js";
+import {
+  getStartValue,
+  getRate,
+  getContributions,
+  calcButton,
+  displayInterest,
+  displayBalance,
+} from "./dom.js";
 
 // initialize arrays to display
 let interestArr = [];
@@ -51,28 +37,11 @@ const calculate = (event) => {
   // get interest and balance arrays
   interestLoop(start, rate, contsArr);
 
-  console.log(`interest: ${interestArr}`);
-  console.log(balanceArr);
-
-  const results = document.getElementById("results");
-
   // display interest
-  let interest;
-  results.append("interest each year");
-  for (let i = 0; i < balanceArr.length; i++) {
-    interest = document.createElement("P");
-    interest.innerHTML = `Year${i + 1} $${interestArr[i]}`;
-    results.append(interest);
-  }
+  displayInterest(interestArr);
 
   //display balance
-  let balance;
-  results.append("balance at start of each year");
-  for (let i = 0; i < balanceArr.length; i++) {
-    balance = document.createElement("P");
-    balance.innerHTML = `Year${i + 1} $${balanceArr[i]}`;
-    results.append(balance);
-  }
+  displayBalance(balanceArr);
 };
 
 calcButton.addEventListener("click", calculate);
